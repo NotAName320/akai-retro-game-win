5c85850e3fb6beaf875b8625a34e15e2070fc969
Rishi Veerepalli <rishi.veerepalli@gmail.com>
2019-04-14 16:34:59 -0700

    Fixed spacing (2)

--- retro-game.c
+++ retro-game.c
@@ -15,42 +15,37 @@ int main(int argc, char **argv) {
 
   SDL_Init(SDL_INIT_VIDEO);
 
-  // Create window
+  // Create renderer and window
   SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, SDL_WINDOW_OPENGL);
-
-  // Create a renderer
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
   
   bool running = true;
   SDL_Event event;
+  
   while (running) {
 
     while (SDL_PollEvent(&event)) {
-      
-      if (event.type == SDL_QUIT)
-	{
-	
+
+      switch (event.type) {
+
+        case SDL_QUIT:
 	  running = false;
+	  break;
+        case SDL_MOUSEBUTTONDOWN:
+	  r = rand() % 255;
+	  g = rand() % 255;
+	  b = rand() % 255;
 
-	}
-      else if (event.type == SDL_MOUSEBUTTONDOWN) {
-
-	r = rand() % 255;
-	g = rand() % 255;
-	b = rand() % 255;
-	
-	SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-
+	  SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+	  break;
+	  
       }
-
-    }
 
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
     
   }
-
 
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
